094347d3d450f566912965758a2b906609efb489
yamilrafart2 <yamilrafart2>
2026-08-04 17:12:33 -0300

    chore: configuración inicial del proyecto

---
+++ src/main/java/com/yamilrafart/apirest_productos/ApirestProductosApplication.java
@@ -0,0 +1,13 @@
+package com.yamilrafart.apirest_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yamilrafart/apirest_productos/ApirestProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamilrafart.apirest_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
